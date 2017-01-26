d5512884e05cc2993c7dfe06d1ee063d7a6c35d2
root <abd.khaja@gmail.com>
2017-01-25 17:58:39 -0800

    Initial Commit

---
+++ SocialLogin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocialLogin
+//
+//  Created by Taiyaba Sultana on 1/25/17.
+//  Copyright © 2017 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
